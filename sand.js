1bbb304c2762a3d4be022edaa197438b8d6172db
Adrian Stadnicki <adrianpatrykstadnicki@gmail.com>
2020-01-25 15:33:55 +0100

    heart shape added

--- sand.js
+++ sand.js
@@ -1,38 +1,57 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const sandColor = "#c2b280";
 let sandDensity = 30;
 let simulationSpeed = 30;
 const settingsForm = document.getElementById('settings-form');
 const sandDensitySlider = document.getElementById('sandDensity');
 const simulationSpeedSlider = document.getElementById('simulationSpeed');
 const canvasDiv = document.getElementById('canvas-div');
+const sandColorInput = document.getElementById('sandColorInput');
+const shapeColorInput = document.getElementById('shapeColorInput');
+const backgroundColorInput = document.getElementById('backgroundColorInput');
+let sandColor = "#c2b280";
+let shapeColor = "black";
+let backgroundColor;
 let simulationInterval;
 let arrayToWorkOn;
 let shapeOptions = Object.keys(sandContainers);
 const shapeOptionsSelect = document.getElementById('shapeOptionsSelect');
 console.log(shapeOptions);
 
-shapeOptions.forEach(optionText => {
-    let option = document.createElement("option");
-    option.text = optionText;
-    shapeOptionsSelect.add(option);
-});
+window.onload = function() {
+    init();
+};
+
+const init = () => {
+    shapeOptions.forEach(optionText => {
+        let option = document.createElement("option");
+        option.text = optionText;
+        shapeOptionsSelect.add(option);
+    });
+};
 
 const copyTwoDimArray = (array) => {
     let newArray = []
-    for (var i = 0; i < array.length; i++) {
-        newArray[i] = [...array[i]];
-    }
+    array.forEach((element, index) => {
+        newArray[index] = [...element];
+    });
     return newArray;
 }
 
-const startSimulation = () => {
+const prepareSimulation = () => {
     clearInterval(simulationInterval);
     sandDensity = sandDensitySlider.value;
     simulationSpeed = simulationSpeedSlider.value;
+    sandColor = sandColorInput.value;
+    shapeColor = shapeColorInput.value;
+    backgroundColor = backgroundColorInput.value;
+
     arrayToWorkOn = copyTwoDimArray(sandContainers[shapeOptionsSelect.value]);
     console.log(`Settings: sand density: ${sandDensity} simultion speed: ${simulationSpeed}`)
+}
+
+const startSimulation = () => {
+    prepareSimulation();
     settingsForm.style.display = "none";
     canvasDiv.style.display = "block";
     drawFromArray(arrayToWorkOn);
@@ -44,9 +63,7 @@ const restartSimulation = () => {
     clearInterval(simulationInterval);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     arrayToWorkOn = copyTwoDimArray(sandContainers[shapeOptionsSelect.value]);
-    drawFromArray(arrayToWorkOn);
-    generateSand(arrayToWorkOn, sandColor);
-    simulationInterval = setInterval(simulation, simulationSpeed);
+    startSimulation();
 }
 
 const showSettings = () => {
@@ -57,11 +74,12 @@ const showSettings = () => {
 const drawFromArray = (array) => {
     const xArraySize = array.length;
     const yArraySize = array[0].length;
+
     for(let x = 0; x < xArraySize; x++) {
         for(let y = 0; y < yArraySize; y++) {
             switch(array[y][x]) {
                 case 1:
-                    ctx.fillStyle = "black";
+                    ctx.fillStyle = shapeColor;
                     ctx.fillRect(x*10, y*10, 10, 10);
                     break;
                 case 2:
@@ -69,7 +87,7 @@ const drawFromArray = (array) => {
                     ctx.fillRect(x*10, y*10, 10, 10);
                     break;
                 case 0:
-                    ctx.fillStyle = "white";
+                    ctx.fillStyle = backgroundColor;
                     ctx.fillRect(x*10, y*10, 10, 10);
                     break;
             }
